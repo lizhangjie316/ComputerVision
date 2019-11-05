6c5ab9542e1564512954370ff97bfa4a6f32915a
lizhangjie316 <lizhangjie316@163.com>
2019-11-05 21:50:54 +0800

    加速神经网络

---
+++ 07-莫凡/pytorch/keen_09_加速神经网络.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : keen_09_加速神经网络.py
+@Time    : 2019/11/5 20:40
+@Author  : Keen
+@Software: PyCharm
+"""
+"""
+1. Stochastic Gradient Descent (SGD)
+2. Momentum
+3. AdaGrad
+4. RMSProp
+5. Adam
+
+"""
+
